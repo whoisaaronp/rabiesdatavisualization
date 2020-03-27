99d0dec0c623b2827c6dbab8482684f46d0d56bd
Aaron Paterson <juniha.aaron@gmail.com>
2020-03-26 23:50:42 -0400

    fixed drag function and infection

--- js/infect.js
+++ js/infect.js
@@ -1,16 +1,5 @@
+
 // stage of infection
-
-//onclick="toggleClass(this)"
-
-// function toggleClass(el) {
-//     var stages = document.qetElementById('stageGroups').children;
-//     for (var i = 0; i < kids.length; i++) {
-//         kids[i].className = "#stageOne";
-//         console.log("happy!");
-//     }
-//     el.className = "#stageTwo";
-// }
-
 
 let stageOne = document.querySelector("#stageOne");
 let stageTwo = document.querySelector("#stageTwo");
@@ -18,31 +7,37 @@ let stageThree = document.querySelector("#stageThree");
 let stageFour = document.querySelector("#stageFour");
 let stageFive = document.querySelector("#stageFive");
 
+// find the content and change 
 let stageGroups = document.querySelector("#toggle-infections");
+
+// toggle the images 
+let imageVar = document.querySelector('#toggle-image');
 
 
 function addStage1() {
-    content.innerHTML = `<p>Stage 1</p>
+    stageGroups.innerHTML = `<p>Stage 1</p>
     <p>An animal is bitten by a rabid animal.</p>
     `
+    imageVar.setAttribute('src', 'stage-1.jpg');
+
 }
 function addStage2() {
-    content.innerHTML = `<p>Stage 2</p>
+    stageGroups.innerHTML = `<p>Stage 2</p>
     <p> Rabies virus from the infected saliva enters the wound.</p>
     `
 }
 function addStage3() {
-    content.innerHTML = `<p>Stage 3</p>
+    stageGroups.innerHTML = `<p>Stage 3</p>
     <p>Rabies virus travels through the nerves to the spinal cord and brain. This process can last approximately 3 to 12 weeks. The animal has no signs of illness during this time.</p>
     `
 }
 function addStage4() {
-    content.innerHTML = `<p>Stage 4</p>
+    stageGroups.innerHTML = `<p>Stage 4</p>
     <p>When it reaches the brain, the virus multiplies rapidly and passes to the salivary glands. The animal begins to show signs of the disease.</p>
     `
 }
 function addStage5() {
-    content.innerHTML = `<p>Stage 5</p>
+    stageGroups.innerHTML = `<p>Stage 5</p>
     <p>The infected animal usually dies within 7 days of becoming sick.</p>
     `
 }
